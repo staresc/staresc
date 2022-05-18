60e342055264aa28c3bdb5124e517c9f2e844e51
5amu <casalinovalerio.cv@gmail.com>
2022-05-18 17:39:04 +0200

    canary token implemented in sshconnection

--- lib/connection/sshconnection.py
+++ lib/connection/sshconnection.py
@@ -1,26 +1,29 @@
 import paramiko
+import os
+import binascii
 from typing import Tuple
 
 from .connection import Connection
 
 
 class SSHConnection(Connection):
-    
+
+    client: paramiko.SSHClient
+
     def __init__(self, connection: str) -> None:
         super().__init__(connection)
-
+        self.client = paramiko.SSHClient()
 
     @staticmethod
     def match_scheme(s: str) -> bool:
         return s == "ssh"
 
 
-    def connect(self) -> None:
+    def connect(self):
         host = self.get_hostname(self.connection)
         port = self.get_port(self.connection)
         usr, pwd = self.get_credentials(self.connection)
 
-        self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         if "/" in pwd:
             self.client.connect(
@@ -37,25 +40,25 @@ class SSHConnection(Connection):
                 password=pwd
             )
 
-    
-    def run(self, cmd: str) -> Tuple[str, str, str]:
 
+    def run(self, cmd: str) -> Tuple[str, str, str]:
         u, p = self.get_root_credentails(self.connection)
         if u != "" and p != "":
             cmd = cmd.replace('\'', '"')
             cmd = f"su {u} -c '{cmd}'"
- 
+
+        canary = binascii.b2a_hex(os.urandom(15))
+        cmd = f"{cmd}; echo {canary}"
         try:
-            #print(f'cmd: {cmd}') #debug
             stdin, stdout, stderr = self.client.exec_command(cmd, get_pty=True, timeout=25)
             if u != "" and p != "":
                 stdout.channel.recv(1024)
                 stdin.write(f"{p}\n")
                 stdin.flush()
 
-            o_stdin  = cmd
-            o_stdout = self.__get_string_from_channel(stdout).strip("\r\n")
-            o_stderr = self.__get_string_from_channel(stderr).strip("\r\n")
+            o_stdin = cmd
+            o_stdout = self.__read_until(stdout.channel, canary).strip("\r\n")
+            o_stderr = self.__read_until(stderr.channel, None).strip("\r\n")
 
         except Exception as e:
             raise e
@@ -63,14 +66,27 @@ class SSHConnection(Connection):
         return o_stdin, o_stdout, o_stderr
 
 
-    def __get_string_from_channel(self, channel):
+    @staticmethod
+    def __read_all(channel: paramiko.Channel) -> bytes:
         data = b''
-        while not channel.channel.exit_status_ready():
-            if channel.channel.recv_ready():
-                data =  channel.channel.recv(1024)
-                prevdata = b" "
-                while prevdata:
-                    prevdata = channel.channel.recv(1024)
-                    data += prevdata
+        if channel.recv_ready():
+            data = channel.recv(1024)
+            prevdata = b" "
+            while prevdata:
+                prevdata = channel.recv(1024)
+                data += prevdata
+        return data
+
+    @staticmethod
+    def __read_until(channel: paramiko.Channel, token) -> str:
+        data = b''
+        # This is stdout
+        if token:
+            while not channel.exit_status_ready() and token not in data:
+                data += SSHConnection.__read_all(channel)
+            return data.split(token)[0].decode('utf-8')
+        # This is stderr
+        while not channel.exit_status_ready():
+            data += SSHConnection.__read_all(channel)
         return data.decode('utf-8')
 
