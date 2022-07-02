3b379c3f54b7b3f9ed023f167cc52997426ef934
5amu <casalinovalerio.cv@gmail.com>
2022-07-02 20:46:27 +0200

    commented exporter

--- staresc/exporter/exporter.py
+++ staresc/exporter/exporter.py
@@ -5,6 +5,12 @@ from staresc.exporter.handlers import StarescHandler
 from staresc.output import Output
 
 class StarescExporter():
+    """Handle Staresc output
+
+    This class is never istanciated, has class methods and an output queue
+    which is handled by a list of handlers objects both when an output is added 
+    to the queue and when an output is taken from the queue
+    """
 
     runs_results: Queue[Output] = Queue()
     handlers: list[StarescHandler] = []
@@ -16,16 +22,19 @@ class StarescExporter():
 
     @classmethod
     def register_handler(cls, handler: StarescHandler):
+        """Called to add an handler to the class"""
         cls.handlers.append(handler)
 
     @classmethod
     def import_output(cls, output: Output) -> None:
+        """Called when an output has to be added to the queue"""
         cls.runs_results.put(output)
         for h in cls.handlers:
             h.import_handler(output)
 
     @classmethod
     def export(cls) -> None:
+        """Called at the end of execution to produce results"""
         outputs: list[Output] = []
         for o in list(cls.runs_results.queue):
             outputs.append(o)
